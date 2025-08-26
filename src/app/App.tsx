815169e077f0d4a2599e2b3e84ae77c5dd0f0b4e
onnkek <onnkek@gmail.com>
2025-08-26 08:41:35 +0500

    test fix footer for IOS

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,7 +5,38 @@ import { Suspense, useEffect, useRef, useState } from 'react';
 import { AppRouter } from './providers/router';
 import { useTheme } from './providers/ThemeProvider/lib/useTheme';
 import { useAppSelector } from 'models/Hook';
+import { Input } from 'shared/ui/Input';
 
+export function useAbsoluteFooter(footerHeight: number) {
+  const [top, setTop] = useState(() => window.innerHeight - footerHeight)
+
+  useEffect(() => {
+    function update() {
+      setTop(window.innerHeight - footerHeight);
+    }
+
+    window.addEventListener("resize", update);
+    window.addEventListener("orientationchange", update);
+
+    if (window.visualViewport) {
+      window.visualViewport.addEventListener("resize", update);
+      window.visualViewport.addEventListener("orientationchange", update);
+    }
+
+    update();
+
+    return () => {
+      window.removeEventListener("resize", update);
+      window.removeEventListener("orientationchange", update);
+
+      if (window.visualViewport) {
+        window.visualViewport.removeEventListener("resize", update);
+        window.visualViewport.removeEventListener("orientationchange", update);
+      }
+    }
+  }, [footerHeight])
+  return { top }
+}
 
 
 
@@ -14,16 +45,42 @@ function App() {
   const [keyboardOpen, setKeyboardOpen] = useState(false);
   const openModal = useAppSelector(state => state.aquarium.modal)
 
+  const { top } = useAbsoluteFooter(80);
 
   return (
-    <div className={classNames('app', {}, [theme])}>
-      <Suspense fallback="">
-        <Navbar />
-        <div className='container'>
-          <AppRouter />
-        </div>
-      </Suspense>
+
+    <div className='test-app'>
+      <div className='test-content'>
+        {/* <AppRouter />
+        <Navbar /> */}
+        <Input type=''></Input>
+        <div className='test-inner'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quam totam tempore odit nemo numquam quas impedit eius dolorem libero hic enim dolore ea saepe, laborum optio id minus soluta!<br /><br /></div>
+        <div className='test-inner'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quam totam tempore odit nemo numquam quas impedit eius dolorem libero hic enim dolore ea saepe, laborum optio id minus soluta!<br /><br /></div>
+        <div className='test-inner'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quam totam tempore odit nemo numquam quas impedit eius dolorem libero hic enim dolore ea saepe, laborum optio id minus soluta!<br /><br /></div>
+        <div className='test-inner'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quam totam tempore odit nemo numquam quas impedit eius dolorem libero hic enim dolore ea saepe, laborum optio id minus soluta!<br /><br /></div>
+        <div className='test-inner'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quam totam tempore odit nemo numquam quas impedit eius dolorem libero hic enim dolore ea saepe, laborum optio id minus soluta!<br /><br /></div>
+        <div className='test-inner'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quam totam tempore odit nemo numquam quas impedit eius dolorem libero hic enim dolore ea saepe, laborum optio id minus soluta!<br /><br /></div>
+        <div className='test-inner'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quam totam tempore odit nemo numquam quas impedit eius dolorem libero hic enim dolore ea saepe, laborum optio id minus soluta!<br /><br /></div>
+        <div className='test-inner'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quam totam tempore odit nemo numquam quas impedit eius dolorem libero hic enim dolore ea saepe, laborum optio id minus soluta!<br /><br /></div>
+        <div className='test-inner'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quam totam tempore odit nemo numquam quas impedit eius dolorem libero hic enim dolore ea saepe, laborum optio id minus soluta!<br /><br /></div>
+        <div className='test-inner'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quam totam tempore odit nemo numquam quas impedit eius dolorem libero hic enim dolore ea saepe, laborum optio id minus soluta!<br /><br /></div>
+        <div className='test-inner'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quam totam tempore odit nemo numquam quas impedit eius dolorem libero hic enim dolore ea saepe, laborum optio id minus soluta!<br /><br /></div>
+        <div className='test-inner'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quam totam tempore odit nemo numquam quas impedit eius dolorem libero hic enim dolore ea saepe, laborum optio id minus soluta!<br /><br /></div>
+      </div>
+      {/* <div className='footer-wrapper'> */}
+      <div className='test-footer' style={{ top: top }}></div>
+      {/* </div> */}
+      {/* <div className='test-footer'></div> */}
     </div>
+
+    // <div className={classNames('app', {}, [theme])}>
+    //   <Suspense fallback="">
+    //     <Navbar />
+    //     <div className='container'>
+    //       <AppRouter />
+    //     </div>
+    //   </Suspense>
+    // </div>
   )
 }
 
